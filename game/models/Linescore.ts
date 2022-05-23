3bbf8e66bb9a22f544a00202550f4a9bda132ec7
Nathanael Mathieu <nmathieu@wesleyan.edu>
2022-05-23 14:37:00 -0400

    Add linescore response types

--- game/models/Linescore.ts
+++ game/models/Linescore.ts
@@ -3,4 +3,70 @@
 /* eslint-disable */
 
 export type Linescore = {
+  copyright?: string,
+  currentInning?: number,
+  currentInningOrdinal?: string,
+  inningState?: string,
+  inningHalf?: string,
+  isTopInning?: false,
+  scheduledInnings?: number,
+  innings?: [ {
+    num?: number,
+    ordinalNum?: string,
+    home?: LinescoreTeamSummary,
+    away?: LinescoreTeamSummary
+  } ],
+  teams?: {
+    home?: LinescoreTeamSummary,
+    away?: LinescoreTeamSummary
+  },
+  defense?: {
+    pitcher?: LinescorePlayer,
+    catcher?: LinescorePlayer,
+    first?: LinescorePlayer,
+    second?: LinescorePlayer,
+    third?: LinescorePlayer,
+    shortstop?: LinescorePlayer,
+    left?: LinescorePlayer,
+    center?: LinescorePlayer,
+    right?: LinescorePlayer,
+    batter?: LinescorePlayer,
+    onDeck?: LinescorePlayer,
+    inHole?: LinescorePlayer,
+    battingOrder?: number,
+    team?: {
+      id?: number,
+      name?: string,
+      link?: string,
+    }
+  },
+  offense?: {
+    batter?: LinescorePlayer,
+    onDeck?: LinescorePlayer,
+    inHole?: LinescorePlayer,
+    first?: LinescorePlayer,
+    pitcher?: LinescorePlayer,
+    battingOrder?: number,
+    team?: {
+      id?: number,
+      name?: string,
+      link?: string,
+    }
+  },
+  balls?: number,
+  strikes?: number,
+  outs?: number,
 };
+
+export type LinescorePlayer = {
+  id?: number,
+  fullName?: string,
+  link?: string,
+};
+
+export type LinescoreTeamSummary = {
+  runs?: number,
+  hits?: number,
+  errors?: number,
+  leftOnBase?: number,
+}
