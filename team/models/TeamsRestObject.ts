7ca791307f1c5a831b7b7dd8c4548737324e6069
Nathanael Mathieu <nmathieu@wesleyan.edu>
2022-05-23 16:20:50 -0400

    Add TeamsRestObject definition

--- team/models/TeamsRestObject.ts
+++ team/models/TeamsRestObject.ts
@@ -3,4 +3,46 @@
 /* eslint-disable */
 
 export type TeamsRestObject = {
+    copyright?: string,
+    teams?: Array<TeamRestObject>
+};
+
+export type TeamRestObject = {
+  allStarStatus?: string,
+  id?: number,
+  name?: string,
+  link?: string,
+  season?: number,
+  venue?: {
+    id?: number,
+    name?: string,
+    link?: string
+  },
+  teamCode?: string,
+  fileCode?: string,
+  abbreviation?: string,
+  teamName?: string,
+  locationName?: string,
+  firstYearOfPlay?: string,
+  league?: {
+    id?: number,
+    name?: string,
+    link?: string
+  },
+  division?: {
+    id?: number,
+    name?: string,
+    link?: string
+  },
+  sport?: {
+    id?: number,
+    link?: string,
+    name?: string
+  },
+  shortName?: string,
+  parentOrgName?: string,
+  parentOrgId?: number,
+  franchiseName?: string,
+  clubName?: string,
+  active?: true
 };
