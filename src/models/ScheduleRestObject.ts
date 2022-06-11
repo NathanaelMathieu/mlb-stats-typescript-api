dbb5a0848161b01638f37cb2cda11525a78e145c
Nathanael Mathieu <nmathieu@wesleyan.edu>
2022-06-11 16:28:49 -0400

    Anything in an array should be it's own type

--- src/models/ScheduleRestObject.ts
+++ src/models/ScheduleRestObject.ts
@@ -4,59 +4,63 @@ export type ScheduleRestObject = {
   totalEvents?: number;
   totalGames?: number;
   totalGamesInProgress?: number;
-  dates?: Array<{
-    date?: string;
-    totalItems?: number;
-    totalEvents?: number;
-    totalGames?: number;
-    totalGamesInProgress?: number;
-    games?: Array<{
-      gamePk?: number;
-      link?: string;
-      gameType?: string;
-      season?: string;
-      gameDate?: string;
-      officialDate?: string;
-      status?: {
-        abstractGameState?: "Preview" | "Live" | "Final";
-        codedGameState?: "S" | "P" | "I" | "F";
-        detailedState?: "Scheduled" | "Pre-Game" | "In Progress" | "Finished";
-        statusCode?: "S" | "P" | "I" | "F";
-        startTimeTBD?: boolean;
-        abstractGameCode?: "P" | "L" | "F";
-      };
-      teams?: {
-        away?: ScheduleRestTeamObject;
-        home?: ScheduleRestTeamObject;
-      };
-      venue?: {
-        id?: number;
-        name?: string;
-        link?: string;
-      };
-      content?: {
-        link?: string;
-      };
-      gameNumber?: number;
-      publicFacing?: boolean;
-      doubleHeader?: string;
-      gamedayType?: string;
-      tiebreaker?: string;
-      calendarEventID?: string;
-      seasonDisplay?: string;
-      dayNight?: string;
-      scheduledInnings?: number;
-      reverseHomeAwayStatus?: boolean;
-      inningBreakLength?: number;
-      gamesInSeries?: number;
-      seriesGameNumber?: number;
-      seriesDescription?: string;
-      recordSource?: string;
-      ifNecessary?: string;
-      ifNecessaryDescription?: string;
-    }>;
-    events?: Array<any>;
-  }>;
+  dates?: Array<ScheduleRestDateObject>;
+};
+
+export type ScheduleRestDateObject = {
+  date?: string;
+  totalItems?: number;
+  totalEvents?: number;
+  totalGames?: number;
+  totalGamesInProgress?: number;
+  games?: Array<ScheduleRestGameObject>;
+  events?: Array<any>;
+};
+
+export type ScheduleRestGameObject = {
+  gamePk?: number;
+  link?: string;
+  gameType?: string;
+  season?: string;
+  gameDate?: string;
+  officialDate?: string;
+  status?: {
+    abstractGameState?: "Preview" | "Live" | "Final";
+    codedGameState?: "S" | "P" | "I" | "F";
+    detailedState?: "Scheduled" | "Pre-Game" | "In Progress" | "Finished";
+    statusCode?: "S" | "P" | "I" | "F";
+    startTimeTBD?: boolean;
+    abstractGameCode?: "P" | "L" | "F";
+  };
+  teams?: {
+    away?: ScheduleRestTeamObject;
+    home?: ScheduleRestTeamObject;
+  };
+  venue?: {
+    id?: number;
+    name?: string;
+    link?: string;
+  };
+  content?: {
+    link?: string;
+  };
+  gameNumber?: number;
+  publicFacing?: boolean;
+  doubleHeader?: string;
+  gamedayType?: string;
+  tiebreaker?: string;
+  calendarEventID?: string;
+  seasonDisplay?: string;
+  dayNight?: string;
+  scheduledInnings?: number;
+  reverseHomeAwayStatus?: boolean;
+  inningBreakLength?: number;
+  gamesInSeries?: number;
+  seriesGameNumber?: number;
+  seriesDescription?: string;
+  recordSource?: string;
+  ifNecessary?: string;
+  ifNecessaryDescription?: string;
 };
 
 export type ScheduleRestTeamObject = {
